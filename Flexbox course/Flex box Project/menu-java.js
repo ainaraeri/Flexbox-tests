ceb9cb9dcc217bb70fb5fef48746867a6f46fcf1
Ainara Erice <117338062+ainaraeri@users.noreply.github.com>
2023-02-21 19:15:41 +0100

    Add files via upload

---
+++ Flexbox course/Flex box Project/menu-java.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("logo");
+const nav = document.getElementById("links-wrapper", "link3-wrapper");
+
+button.addEventListener('click', () => {
+  links-wrapper.classList.toggle('show');
+});
